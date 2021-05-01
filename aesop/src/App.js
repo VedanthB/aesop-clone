3b5da53cd525f5e0943dc2c5d67e4915fba50edf
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-01 11:53:49 +0530

    ADD: item

--- aesop/src/App.js
+++ aesop/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Item from './components/Item';
 import Rows from './components/Rows';
 
 function App() {
@@ -16,7 +17,14 @@ function App() {
         <Rows />
 
         {/* item  */}
-
+        <Item 
+         src='https://www.aesop.com/u1nb1km7t5q7/73c4JT56oX3r1xn2UVqAzd/41db81db3809032ac0ab82b9d6cdc3d1/Aesop_Skin_Care_Plus_2021_Web_Range_Page_Secondary_Mid_GL_Desktop_2560x1440px.jpg'
+         h3='Knowledge is power'
+         h2='Aesop on Vitamins'
+         p='
+         The science behind some of the ingredients we return to time and again for their unique characteristics—be they soothing, calming, balancing, nourishing or anti-oxidant.'
+         button='Discover Vitamins'         
+        />
         {/*  Rows with Products */}
 
         {/* item  */}
