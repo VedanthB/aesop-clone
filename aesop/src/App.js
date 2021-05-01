e70ed125f4ccff9cb43f634d058abd2153674afb
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-01 09:42:16 +0530

    initial cra

---
+++ aesop/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+        {/* Header */}
+        
+        {/* Home Page */}
+
+        {/* Rows with Products */}
+
+        {/* item  */}
+
+        {/*  Rows with Products */}
+
+        {/* item  */}
+
+        {/* item inverted */}
+
+        {/* footer */}
+    </div>
+  );
+}
+
+export default App;
